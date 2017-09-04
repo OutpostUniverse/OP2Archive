cfd9d623bb68a6aabc81dd7ce2a49e7ca5218494
Brett208 <UseOutpostUniverse (Vagabond)>
2017-09-03 22:10:20 -1000

    Added ConsoleSettings.h. (Missed file in previous commit)

---
+++ ConsoleSettings.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "OP2Utility.h"
+#include <vector>
+#include <string>
+
+struct ConsoleSettings
+{
+	string destDirectory = "./";
+	string sourceDirectory = "./";
+	bool overwrite = false;
+	bool quiet = false;
+	bool helpRequested = false;
+	Archives::CompressionType compression = Archives::CompressionType::Uncompressed;
+};
+
+enum class ConsoleCommand
+{
+	List,
+	Find,
+	Extract,
+	Create,
+	Help,
+	Add,
+	Remove
+};
+
+struct ConsoleArgs
+{
+	ConsoleCommand consoleCommand = ConsoleCommand::Help;
+	ConsoleSettings consoleSettings;
+	vector<string> paths;
+};
